ce5db0651d04b37da11fd3533311a28271bebfaa
Andres Osorio <androideosorio@me.com>
2023-12-05 22:56:25 +0100

    add CLI arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use clap::Parser;
+
+/// The advent of code solutions
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// The puzzle to run
+    puzzle: String,
+
+    /// The part of the specific day to run
+    #[arg(short, long, default_value_t = 1)]
+    part: u8,
+}
+
 fn main() {
+    let cli = Cli::parse();
+    let name = cli.puzzle;
+    let puzzle_part = cli.part;
+
+    println!("Puzzle to run: {name}");
+    println!("Value for part: {}", puzzle_part);
     println!("Hello, world!");
 }
